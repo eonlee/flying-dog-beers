ae8070669218a979ce8c6291a21555b1c079194a
eonlee <39643006+eonlee@users.noreply.github.com>
2020-03-05 06:31:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import plotly.graph_objects as go
 def get_codedf():
     url_main = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13'
     code_df = pd.read_html(url_main, header=0)[0]
-    code_df.종목코드 = code_df.종목코드.map('{:06d}'.format)
+    # code_df.종목코드 = code_df.종목코드.map('{:06d}'.format)
     code_df = code_df[['회사명', '종목코드']]
     code_df = code_df.rename(columns={'회사명': 'name', '종목코드': 'code'})
     return code_df
